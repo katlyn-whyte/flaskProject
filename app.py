3483ab437b25405d678766323c688b06de5bc806
katlyn <katlyn.whyte@my.jcu.edu.au>
2022-11-30 22:26:32 +1000

    Update app with temperature conversions

--- app.py
+++ app.py
@@ -14,5 +14,22 @@ def greet(name=""):
     return f'hello {name}'
 
 
+@app.route('/calculate celsius to fahrenteit')
+@app.route('/celsius/<celsius>')
+def celsius(celsius=" "):
+
+
+def calculate_fahrenheit(fahrenheit):
+    """Calculating fahrenheit to celsius"""
+    fahrenheit_to_celsius = 5 / 9 * (fahrenheit - 32)
+    return fahrenheit_to_celsius
+
+
+def calculate_celsius(celsius):
+    """Calculating celsius to fahrenheit"""
+    celsius_to_fahrenheit = celsius * 9.0 / 5 + 32
+    return celsius_to_fahrenheit
+
+
 if __name__ == '__main__':
     app.run()
